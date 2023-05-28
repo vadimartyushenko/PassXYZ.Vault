16a25de90de0e246a6648070101a0d617f59cde2
Vadim Artyushenko <artushenkomail@mail.ru>
2023-05-29 00:03:22 +0700

    Добавьте файлы проекта.

---
+++ PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace PassXYZ.Vault;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+		var window = base.CreateWindow(activationState);
+        var nameApp = "PassXYZ.Vault.App";
+        window.Created += (s , e) => {
+            Debug.WriteLine($"{nameApp}: 1. Created event");
+        };
+        window.Activated += (s, e) => {
+            Debug.WriteLine($"{nameApp}: 2. Activated event");
+        };
+        window.Deactivated += (s, e) => {
+            Debug.WriteLine($"{nameApp}: 3. Created event");
+        };
+        window.Stopped += (s, e) => {
+            Debug.WriteLine($"{nameApp}: 4. Stopped event");
+        };
+        window.Resumed += (s, e) => {
+            Debug.WriteLine($"{nameApp}: 5. Resumed event");
+        };
+        window.Destroying += (s, e) => {
+            Debug.WriteLine($"{nameApp}: 6. Destroying event");
+        };
+        return window;
+    }
+}
